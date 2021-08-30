aa2fc0b22b9ca638371492bd096a1b66b31ad9f4
kanatamon <kantapon.sritiyot@gmail.com>
2021-08-30 21:08:29 +0700

    feat: add minimal landing-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,43 @@
 import * as React from 'react'
-import {styled, useStyletron} from 'styletron-react'
+import {Card, StyledBody, StyledAction} from 'baseui/card'
 import {Button} from 'baseui/button'
 import Link from 'next/link'
 
 import * as QuestionnairesUtils from '../questionnaires-utils'
 
-// statically styled component
-const Title = styled('h1', {
-  color: 'red',
-  fontSize: '82px',
-})
-
-// dynamically styled component
-const SubTitle = styled('h2', ({$size}) => ({
-  color: 'blue',
-  fontSize: `${$size}px`,
-}))
-
 export default function LandingPage() {
-  // an alternative hook based API
-  const [css] = useStyletron()
   const getStartedQuestionLink =
     QuestionnairesUtils.generateGetStartedQuestionLink()
 
   return (
-    <div>
-      <Title>Landing Page</Title>
-      <SubTitle $size={50}>Subtitle</SubTitle>
-      <p className={css({fontSize: '32px'})}>Styled by hook</p>
-      <Link href={getStartedQuestionLink} passHref>
-        <Button $as="a">Get Started</Button>
-      </Link>
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'grid',
+        placeContent: 'center',
+      }}
+    >
+      <Card
+        title="กิจกรรมโครงการวิจัย มหาวิทยาลัยเชียงใหม่"
+        overrides={{Root: {style: {width: '328px'}}}}
+      >
+        <StyledBody>
+          ขอเชิญชวนผู้ร่วมกิจกรรมตอบแบบสอบถาม ปัญหา (Pain Point) และความต้องการ
+          (Gain Point) ของ Gen Z ในด้านรูปแบบการจัดการเรียนรู้และหลักสูตร
+          ทั้งนี้ผู้ร่วมตอบแบบสอบถามในกิจกรรมมีสิทธิ์ได้ลุ้นรับของรางวัลจากทางโครงการด้วย
+        </StyledBody>
+        <StyledAction>
+          <Link href={getStartedQuestionLink} passHref>
+            <Button
+              overrides={{
+                BaseButton: {style: {width: '100%'}},
+              }}
+            >
+              เริ่มทำแบบสอบถาม
+            </Button>
+          </Link>
+        </StyledAction>
+      </Card>
     </div>
   )
 }
