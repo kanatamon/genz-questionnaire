15e1c7da7fe6deadb88a5920e70a8de2888b23db
kanatamon <kantapon.sritiyot@gmail.com>
2021-09-03 15:19:52 +0700

    fix: crossOrigin warning

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,7 +37,11 @@ class MyDocument extends Document {
             />
           ))}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin={true}
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;400;700&display=swap"
             rel="stylesheet"
