9a4515e9a03700f87b1bb775d231c86c59331361
kanatamon <kantapon.sritiyot@gmail.com>
2021-10-21 11:25:15 +0700

    fix(warning): use string for crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,7 +40,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin={true}
+            crossOrigin="true"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;400;700&display=swap"
