49af9135f6c6e1b3dcefef11098ab6030df5477f
kanatamon <kantapon.sritiyot@gmail.com>
2021-11-05 14:52:44 +0700

    feat: setting responsive viewport

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,6 +36,10 @@ class MyDocument extends Document {
               key={i}
             />
           ))}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
