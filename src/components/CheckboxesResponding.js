07141b2c7e3a9549a31b681012dbae410c5c8499
kanatamon <kantapon.sritiyot@gmail.com>
2021-11-29 17:01:54 +0700

    feat(new-schema-compatible): store all options with weight selector

--- src/components/CheckboxesResponding.js
+++ src/components/CheckboxesResponding.js
@@ -8,54 +8,17 @@ import * as ClientMemory from '../client-memory'
 import {RespondingCommon} from './RespondingCommon'
 
 function CheckboxesResponding({question, onValidate = () => {}}) {
-  const [checkboxes, setCheckboxes] = React.useState([])
-  const [otherText, setOtherText] = React.useState('')
+  const [checkboxes, setCheckboxes] = React.useState(
+    initCheckboxesFromMemory(question),
+  )
+  const [otherText, setOtherText] = React.useState(
+    initOtherTextFromMemory(question.id),
+  )
   const onValidateRef = React.useRef(onValidate)
 
   React.useEffect(function asyncOnValidateCallback() {
     onValidateRef.current = onValidate
   })
-
-  React.useEffect(
-    function initValuesToWhateverSavedInMemoryWhenQuestionChanged() {
-      const memoryResponding = ClientMemory.getRespondingByQuestionId(
-        question.id,
-      )
-      const memoryValues = memoryResponding?.respondingOptions ?? []
-
-      const checkboxRespondings = memoryValues.filter(
-        memoryValue => !!memoryValue.respondingText,
-      )
-
-      const {displayAsCheckboxOptions, initialCheckboxes} =
-        generateInitialCheckboxesValues(question)
-
-      if (checkboxRespondings.length > 0) {
-        const respondingTexts = checkboxRespondings.map(
-          responding => responding.respondingText,
-        )
-
-        const memoryCheckboxes = displayAsCheckboxOptions.map(option =>
-          respondingTexts.includes(option.title),
-        )
-
-        setCheckboxes(memoryCheckboxes)
-      } else {
-        setCheckboxes(initialCheckboxes)
-      }
-
-      const otherTextResponding = memoryValues.find(
-        memoryValue => !!memoryValue.respondingOtherText,
-      )
-
-      if (otherTextResponding) {
-        setOtherText(otherTextResponding.respondingOtherText)
-      } else {
-        setOtherText('')
-      }
-    },
-    [question],
-  )
 
   React.useEffect(
     function validateValuesWhenChanged() {
@@ -70,40 +33,33 @@ function CheckboxesResponding({question, onValidate = () => {}}) {
 
   React.useEffect(
     function saveValuesToMemoryWhenChanged() {
-      const {displayAsCheckboxOptions} =
-        generateInitialCheckboxesValues(question)
+      const {displayAsCheckboxOptions} = generateInitialCheckboxesValues(
+        question.options,
+      )
 
-      const checkedRespondingOptions = checkboxes
-        .map((checked, checkboxIndex) => {
-          if (checked) {
-            return {
-              respondingText: displayAsCheckboxOptions[checkboxIndex].title,
-              weight: null,
-              isOther: false,
-              respondingOtherText: null,
-            }
+      const checkedRespondingOptions = displayAsCheckboxOptions.map(
+        (option, optionIndex) => {
+          return {
+            respondingText: option.title,
+            weight: +checkboxes[optionIndex],
+            isOther: false,
+            respondingOtherText: null,
           }
+        },
+      )
 
-          return null
-        })
-        .filter(respondingOption => !!respondingOption)
-
-      let patchedRespondingOptions = [...checkedRespondingOptions]
-
-      if (otherText) {
-        patchedRespondingOptions = [
-          ...patchedRespondingOptions,
-          {
-            respondingText: null,
-            weight: null,
-            isOther: true,
-            respondingOtherText: otherText,
-          },
-        ]
-      }
+      let savingRespondingOptions = [
+        ...checkedRespondingOptions,
+        {
+          respondingText: null,
+          weight: otherText?.trim() ? 1 : 0,
+          isOther: true,
+          respondingOtherText: otherText?.trim() ? otherText.trim() : null,
+        },
+      ]
 
       ClientMemory.patchRespondingByQuestionId(question.id, {
-        respondingOptions: patchedRespondingOptions,
+        respondingOptions: savingRespondingOptions,
       })
     },
     [checkboxes, otherText, question],
@@ -123,7 +79,9 @@ function CheckboxesResponding({question, onValidate = () => {}}) {
     setOtherText(currentOtherAnswer)
   }
 
-  const {displayAsCheckboxOptions} = generateInitialCheckboxesValues(question)
+  const {displayAsCheckboxOptions} = generateInitialCheckboxesValues(
+    question.options,
+  )
 
   return (
     <RespondingCommon question={question}>
@@ -167,8 +125,40 @@ function CheckboxesResponding({question, onValidate = () => {}}) {
   )
 }
 
-function generateInitialCheckboxesValues(question) {
-  const displayAsCheckboxOptions = question.options.filter(
+function initCheckboxesFromMemory(question) {
+  const memoryResponding = ClientMemory.getRespondingByQuestionId(question.id)
+
+  const memoryValues = memoryResponding?.respondingOptions ?? []
+
+  if (memoryValues.length > 0) {
+    const memoryCheckboxes = memoryValues.map(responding => !!responding.weight)
+
+    return memoryCheckboxes
+  }
+
+  const {initialCheckboxes} = generateInitialCheckboxesValues(question.options)
+
+  return initialCheckboxes
+}
+
+function initOtherTextFromMemory(questionId) {
+  const memoryResponding = ClientMemory.getRespondingByQuestionId(questionId)
+
+  const memoryValues = memoryResponding?.respondingOptions ?? []
+
+  const otherTextResponding = memoryValues.find(
+    memoryValue => !!memoryValue.respondingOtherText,
+  )
+
+  if (otherTextResponding) {
+    return otherTextResponding.respondingOtherText
+  }
+
+  return ''
+}
+
+function generateInitialCheckboxesValues(options) {
+  const displayAsCheckboxOptions = options.filter(
     option => option.type === undefined,
   )
   const initialCheckboxes = displayAsCheckboxOptions.map(() => false)
